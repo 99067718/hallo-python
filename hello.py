565d0ee42453f3aa00be0406a12fa50a08f135d1
unknown <99067718@mydavinci.nl>
2021-09-07 13:31:01 +0200

    hello python

---
+++ hello.py
@@ -0,0 +1,13 @@
+#Naam: Emiel 't Lam
+#StudentenNummer: 99067718
+
+print("Hello wereld van…   ")                                 
+print("")
+print("")
+print("    pppp    y     y   ttttttt   h   h    oooo   nnnn  ")
+print("    p   p    y   y       t      h   h   o    o  n   n")
+print("    p   p     y y        t      h   h   o    o  n   n")
+print("    pppp       y         t      hhhhh   o    o  n   n")
+print("    p          y         t      h   h   o    o  n   n")
+print("    p          y         t      h   h   o    o  n   n")
+print("    p          y         t      h   h    oooo   n   n")
